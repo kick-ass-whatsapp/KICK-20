ac6be62e75527b2aa92bd6e741dc9b215255caac
kick-ass-whatsapp <kickassbot55@gmail.com>
2021-04-18 02:58:12 -0400

    First commit

---
+++ src/18.js
@@ -0,0 +1,10 @@
+[
+    {
+    "teks": "Nome : Danca\nFoto : 18\nVideo : 13\nTamanho : 100 MB\nDuração : - Minutos\nQualidade : HD+\nAvaliação : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1hDxgMnQ4ps2DkVhFyc51ApdsavGfqs3B/view\nSenha do arquivo : AA18+#38",
+    "image": "https://i.ibb.co/s9dJRr1/image.png"
+    },
+    {
+    "teks": "Nome : Mia Callista\n\naltura : 1,57 m\n\nNascimento : 10 de fevereiro de 1993\n\nidade : 28n\n\nInício da atividade : 2014\n\nOutros nomes : Mia Callista; (em árabe: ميا خليفة)",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQljRIYqjixhog6NgNvs5DQowLvI4bk-LrXg&usqp=CAU"
+    }
+]
